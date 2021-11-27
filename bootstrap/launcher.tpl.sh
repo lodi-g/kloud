a097cb23241be6d199756cdbee154e3b10d106c5
Grégoire Lodi <gregoire.lodi@gmail.com>
2021-11-27 13:10:03 +0100

    Change internet detection from ping to DNS resolution

--- bootstrap/launcher.tpl.sh
+++ bootstrap/launcher.tpl.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+cloud_url="%s"
 internal_dir="%s"
 sync_dir="%s"
 sd_sync_dir="/mnt/sd/KloudSync"
@@ -12,8 +13,22 @@ function log() {
 
 log "Launcher started. internal_dir=$internal_dir, sync_dir=$sync_dir, sd_sync_dir=$sd_sync_dir"
 
-# Make sure we have internet
-ping -c2 1.1.1.1 > /dev/null
+# Wait for DNS resolution to come in
+for i in $(seq 0 60); do
+  nslookup "$cloud_url"
+  if [ $? -eq 0 ]; then
+    log "Got DNS resolution"
+    break
+  fi
+
+  log "No DNS resolution, sleeping"
+  sleep 1
+done
+
+if [ $i -eq 60 ]; then
+  log "Too many attempts for DNS resolution, exiting"
+  exit 1
+fi
 
 # Run kloud
 log "Starting kloud"
@@ -23,12 +38,16 @@ log "kloud exited with code $exit_code"
 
 if [ $exit_code != 0 ]; then
   log "kloud exited with an error, not performing library refresh"
-  exit 0
+  exit 1
 fi
 
 # Trick to refresh library: https://github.com/wernerb/kobo-wget-sync/blob/master/src/usr/local/wget-sync/refresh_library.sh
 mkdir -p "$sd_sync_dir"
-mount "$kloud_library_folder" "$sd_sync_dir"
+mount "$sync_dir" "$sd_sync_dir"
+if [ $? -ne 0 ]; then
+  log "Failed to perform library refresh (mount)"
+  exit 1
+fi
 
 log "Refreshing library"
 echo sd add /dev/mmcblk1p1 >> /tmp/nickel-hardware-status
